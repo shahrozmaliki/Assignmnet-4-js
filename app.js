bd8866d6dc637955955d9ba41c39fa196c2a1801
Shahroz Aisf <pubg71369@gmail.com>
2024-12-28 15:35:07 +0500

    Arrays

---
+++ app.js
@@ -0,0 +1,27 @@
+var sk = [10,20,30,40,50 ];  
+
+  for(var a = 0; a <= 4; a++){
+    document.write(sk[a] + "<br>");
+
+  }
+
+
+
+// var sk = [10,20,30,40,50];
+
+// document.write("<ul>")
+//   for(var dj = 0;  dj < 5 ; dj++){
+//     document.write("<li" + sk[dj] + "<li>");
+//   }
+//   document.write("<ul>");
+    
+
+
+
+
+ 
+
+
+
+
+  
